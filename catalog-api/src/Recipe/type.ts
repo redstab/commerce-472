d1ec5e7e24b0bf11a93ccab639e93bfb9ebebb88
Jens Lindström <jens.swedish@gmail.com>
2022-01-06 19:14:14 +0100

    catalog-api: add typegraphql

---
+++ catalog-api/src/Recipe/type.ts
@@ -0,0 +1,10 @@
+import { Field, ID, ObjectType } from "type-graphql";
+
+@ObjectType()
+export class Post {
+    @Field(() => ID)
+    id!: number;
+
+    @Field()
+    title!: string;
+}
--- catalog-api/src/index.ts
+++ catalog-api/src/index.ts
@@ -1,10 +1,25 @@
+import "reflect-metadata";
 import { PrismaClient } from '@prisma/client'
+import { ApolloServer } from 'apollo-server'
+import { buildSchema } from 'type-graphql'
+import { PostResolver } from './Recipe/resolver'
+import { ApolloServerPluginLandingPageGraphQLPlayground } from "apollo-server-core";
 
 const prisma = new PrismaClient()
 
 async function main() {
-  const posts = await prisma.post.findMany()
-  console.log(posts)
+  const schema = await buildSchema({
+    resolvers: [PostResolver]
+  })
+  
+  const server = new ApolloServer({
+    schema,
+    plugins: [ApolloServerPluginLandingPageGraphQLPlayground]
+  })
+
+  server.listen().then(({ url }) => {
+    console.log(`🚀  Server ready at ${url}`);
+  });
 }
 
 main()
